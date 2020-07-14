b7d628bfb27fe6c279be8686e6c86b6448220121
Nicole Zhu <n.zhu5646@berkeley.edu>
2020-07-13 21:35:36 -0400

    finished get_proj_id, built supporting utils to parse through API response

---
+++ tests/test_add_video.py
@@ -0,0 +1,75 @@
+import unittest
+import sys
+import os
+import warnings
+from expected_response import xr_nodes, xr_pivot1, xr_pivot2
+sys.path.append('../check_api')
+from meedan_interface import MeedanAPI
+import util
+
+class TestAPI(unittest.TestCase):
+
+    def setUp(self):
+        self.key = os.environ.get('MEEDAN_KEY')
+        self.meedan_api = MeedanAPI(self.key)
+        warnings.simplefilter("ignore", ResourceWarning)
+
+    def test_strip(self):
+        sample_query = '''query {
+          team(slug: "ischool-hrc") {
+            projects {
+              edges {
+                node {
+                  title
+                  id
+                  dbid
+                }
+              }
+            }
+          }
+        }
+        '''
+        response = self.meedan_api.execute(sample_query)
+        nodes = util.strip(response)
+        self.assertEqual(nodes, xr_nodes, "Strip function has failed")
+
+        sample_dict = {"data": [{
+            "team": {
+                "edges": [{
+                    "node": {
+                        "name": "Nicole"
+                            }
+                        }]
+                    }
+            },
+            {
+                "team": {
+                    "edges": [{
+                        "node": {
+                            "name": "Nicole"
+                                }
+                            }]
+                        }
+                }]
+        }
+        expected_list = [{'node': {'name': 'Nicole'}}, {'node': {'name': 'Nicole'}}]
+        self.assertEqual(util.strip(sample_dict), expected_list, "Strip function failed on nested dictionaries")
+
+    def test_pivot_dict(self):
+        #Test pivoting where the value is a single entity
+        self.assertEqual(util.pivot_dict(xr_nodes, "title", "dbid"), xr_pivot1, "Pivot function failed")
+        #Test pivoting where the value is a list
+        self.assertEqual(util.pivot_dict(xr_nodes, "title", ["dbid", "id"]), xr_pivot2, "Pivot function failed on value lists")
+
+    def test_get_list_id(self):
+        id = self.meedan_api.get_proj_id("ischool-hrc", "#Nicole")
+        self.assertEqual(id, "3141")
+        id = self.meedan_api.get_proj_id("ischool-hrc", "#Vyoma")
+        self.assertEqual(id, "3135")
+
+    #TODO: Finish fleshing out test once add_video is implemented
+    # def test_add(self):
+    #     response = self.meedan_api.add_video("C2xel6q0yao", "#Wietske")
+
+if __name__ == '__main__':
+    unittest.main()
